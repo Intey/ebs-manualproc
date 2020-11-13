114d2288f392ab13d4ee7bb7cb3ca6f180318067
ane4ka0o <ane4ka0o@yandex.ru>
2020-11-13 22:52:43 +0300

    proto

--- main.go
+++ main.go
@@ -1,4 +1,30 @@
+package main
+
 import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/streadway/amqp"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func connect() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+}
+
 func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
